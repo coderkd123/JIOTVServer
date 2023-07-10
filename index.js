16c1757a5c82a3ca5cf0c4735bb5f71e29ebcd40
coderkd123 <124048609+coderkd123@users.noreply.github.com>
2023-07-10 23:07:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const app = express();
 app.use(express.urlencoded({ extended: true}));
 import fs from 'fs';
 app.use(express.json());
-const PORT = process.env.PORT || 3000;
 import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
@@ -61,20 +60,8 @@ import { handler } from "./build/handler.js";
 
 app.use(handler);
 
-app.listen(PORT, () => {
-  console.log("===================================================================");
-  console.log(chalk.green("THIS SERVER IS 100% FREE. PLEASE DON'T PAY ANYONE."));
-  console.log(chalk.green("STRICT ACTION WILL BE TAKEN AGAINST THOSE WHO ARE SELLING THIS."));
-  console.log(chalk.green("IF ANYONE TRYING TO SELL IT, PLEASE REPORT IT ON OUR DISCORD SERVER\nhttps://discord.gg/suyzkCQKhC"));
-  console.log(chalk.green("Please Stare Repo https://github.com/dhruv-2015/JIOTVServer"));
-  console.log(chalk.green("VERSION 2.8.6"));
-    console.log(chalk.green("980+ CHANNELS AVAILABLE. MORE FEATURES WILL COME SOON. STAY TUNED"));
+app.listen( () => {
     console.log("===================================================================");
     console.log(chalk.red("       __ ____ ____     ______ _    __ \n      / //  _// __ \\   /_  __/| |  / / \n __  / / / / / / / /    / /   | | / /  \n/ /_/ /_/ / / /_/ /    / /    | |/ /   \n\\____//___/ \\____/    /_/     |___/    \n                                "));
     console.log("===================================================================");
-    console.log(`TV server is running on port ${PORT}`);
-    console.log(`Please open http://localhost:${PORT}/login to login and get playlist if running server for the first time`);
-    // console.log(chalk.red("need to login every 24 hours even if you are already logged in"));
-    console.log("If facing any errors, please login from portal again");
-    console.log("you can use server m3u8 links in other websites and apps");
 });
